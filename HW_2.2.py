94d18f31c99e5e71af299be32237b972252b1a2b
kilimovaann <annkilimova85@gmail.com>
2024-06-06 21:55:01 +0300

    Commit1

--- HW_2.2.py
+++ HW_2.2.py
@@ -1,3 +1,3 @@
-a = int(input('Enter a 4-digit number: '))
+a = int(input('Enter a 5-digit number: '))
 
-print(f'{str(a % 10) + str(a // 10 % 10) + str(a // 100 % 10) + str(a // 1000 % 10)}')
+print(f'{str(a % 10) + str(a // 10 % 10) + str(a // 100 % 10) + str(a // 1000 % 10) + str(a // 10000 % 10)}')
