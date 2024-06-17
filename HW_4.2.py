196befa34338c8469a8e691348f81565a3e7f8a9
kilimovaann <annkilimova85@gmail.com>
2024-06-17 21:25:37 +0100

    Commit1

--- HW_4.2.py
+++ HW_4.2.py
@@ -1,10 +1,16 @@
 my_list = [0, 1, 7, 2, 4, 8]
+# my_list = [1, 3, 5]
+# my_list = [6]
+# my_list = []
+
 my_sum = 0
 
 for element in range(0, len(my_list), 2):
+
     my_sum += my_list[element]
 
-my_sum *= my_list[len(my_list) - 1]
+if len(my_list) > 0:
+    my_sum *= my_list[len(my_list) - 1]
 
 print(my_sum)
 
