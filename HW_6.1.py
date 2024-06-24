e0f0fbb301b469c34d5fd2c925da6e6182ceb6d6
kilimovaann <annkilimova85@gmail.com>
2024-06-24 22:00:00 +0100

    Commit1

--- HW_6.1.py
+++ HW_6.1.py
@@ -2,7 +2,7 @@ import string
 
 my_str = input('Enter two letters hyphenated: ').split('-')
 first_letter = string.ascii_letters.find(my_str[0])
-last_letter = string.ascii_letters.find(my_str[1])
+last_letter = string.ascii_letters.find(my_str[-1])
 
 print(string.ascii_letters[first_letter : last_letter + 1])
 
