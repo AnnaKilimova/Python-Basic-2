3540b877e28ee2e5e4a5f22da700db74398a1be4
kilimovaann <annkilimova85@gmail.com>
2024-06-24 21:57:34 +0100

    Commit1

---
+++ HW_6.1.py
@@ -0,0 +1,8 @@
+import string
+
+my_str = input('Enter two letters hyphenated: ').split('-')
+first_letter = string.ascii_letters.find(my_str[0])
+last_letter = string.ascii_letters.find(my_str[1])
+
+print(string.ascii_letters[first_letter : last_letter + 1])
+
