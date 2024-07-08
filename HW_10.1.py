08193fda68002845be5ca4d4001162581873df26
kilimovaann <annkilimova85@gmail.com>
2024-07-08 21:03:21 +0100

    Commit1

---
+++ HW_10.1.py
@@ -0,0 +1,13 @@
+from inspect import isgenerator
+def pow(x):
+    return x ** 2
+def some_gen(begin, end, func):
+    for _ in range(end):
+        yield begin
+
+        begin = pow(begin)
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
