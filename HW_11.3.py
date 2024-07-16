9ffd929acf53bcbc10f645c3b5b237d9b74ec652
kilimovaann <annkilimova85@gmail.com>
2024-07-16 01:32:18 +0100

    Commit1

---
+++ HW_11.3.py
@@ -0,0 +1,12 @@
+def is_even(number):
+    scope = (_ for _ in range(2, 10, 2))
+
+    for element in scope:
+        if element == int(str(number)[-1]):
+            return True
+    return False
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('ok')
