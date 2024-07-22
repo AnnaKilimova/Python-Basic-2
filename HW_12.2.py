174e4a9db0577d67143ebf318d07d211df74c6d4
kilimovaann <annkilimova85@gmail.com>
2024-07-22 10:24:07 +0100

    Commit1

--- HW_12.2.py
+++ HW_12.2.py
@@ -1,61 +1,53 @@
 class Item:
+
     def __init__(self, name, price, description, dimensions):
         self.price = price
         self.description = description
         self.dimensions = dimensions
         self.name = name
+
     def __str__(self):
         return f'{self.name}, price: {self.price}'
+
 class User:
+
     def __init__(self, name, surname, numberphone):
         self.name = name
         self.surname = surname
         self.numberphone = numberphone
+
     def __str__(self):
         return f'{self.name} {self.surname}'
+
 class Purchase:
     def __init__(self, user):
         self.products = {}
         self.user = user
         self.total = 0
+
     def add_item(self, item, cnt):
         self.products[item] = cnt
 
-        for i in self.products.items():
-            print(item.name, cnt)
     def __str__(self):
-        return f'''
-User: {self.user}
+        items = ''
+        for item, cnt in self.products.items():
+            items += f'{str(item.name)}: {cnt} pcs. \n'
+        return f'''User: {self.user}
 Items:
-
-{self.products.items()}
-
-{self.get_total()}'''
-
-
-
-        #return f"""User: {self.user}
-# Items:
-# {self.products}
-# {self.products.items()}
-# lemon: 4 pcs.
-# apple: 20 pcs.
-# """
+{items}
+'''
     def get_total(self):
         self.total = 0
         for key, cnt in self.products.items():
             self.total += key.price * cnt
-            print(f'key.price = {key.price}, cnt = {cnt}')
         return self.total
-
-
 
 lemon = Item('lemon', 5, "yellow", "small", )
 apple = Item('apple', 2, "red", "middle", )
-print(lemon)  # lemon, price: 5
+print(lemon)
 
 buyer = User("Ivan", "Ivanov", "02628162")
-print(buyer)  # Ivan Ivanov
+print(buyer)
 
 cart = Purchase(buyer)
 cart.add_item(lemon, 4)
