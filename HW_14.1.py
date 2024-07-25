cc0f1e70d62980a28d54b02df04aa61d808019b0
kilimovaann <annkilimova85@gmail.com>
2024-07-25 23:35:39 +0100

    Commit1

--- HW_14.1.py
+++ HW_14.1.py
@@ -4,6 +4,7 @@ class Human:
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
+
     def __str__(self):
         return f'{self.gender}, {self.age}, {self.first_name} {self.last_name}'
 
@@ -11,6 +12,7 @@ class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
+
     def __str__(self):
         return f'{self.gender} {self.age} {self.first_name} {self.last_name} {self.record_book}'
 
@@ -18,21 +20,25 @@ class Group:
     def __init__(self, number):
         self.number = number
         self.group = set()
+
     def add_student(self, student):
         if len(self.group) == 10:
             raise ValueError('User exception')
 
         self.group.add(student)
+
     def delete_student(self, last_name):
         if self.find_student(last_name) != None:
             for i in self.group:
                 if last_name in str(i):
                     self.group.discard(i)
                     return self.group
+
     def find_student(self, last_name):
         for student in self.group:
             if last_name in str(student):
                 return student
+
     def __str__(self):
         all_students = ''
         self.number = 0
